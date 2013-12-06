50cb395cbc2abf8348d8e2325a9586d8499bfb34
Goblom <goblom@me.com>
2013-12-06 00:24:41 -0800

    Preparing

---
+++ src/main/java/goblom/tenjava/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2013 Goblom.
+ */
+
+package goblom.tenjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Goblom
+ */
+public class Main extends JavaPlugin {
+    
+}
